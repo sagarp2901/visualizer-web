dd035840bf8b53405ac225706429b1c72462725a
sagarp2901 <sagar2901@hotmail.com>
2020-03-22 17:07:42 -0400

    Updates

---
+++ precache-manifest.694ec83978ced05a4a2794f3b5d7f9ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e0bcbab96ad3a40673876914a8b71d",
+    "url": "/sagarp2901/covid-visualizer/index.html"
+  },
+  {
+    "revision": "850108ed5982bd159889",
+    "url": "/sagarp2901/covid-visualizer/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "3d0fbbca676867aa7f58",
+    "url": "/sagarp2901/covid-visualizer/static/css/main.30d6997f.chunk.css"
+  },
+  {
+    "revision": "850108ed5982bd159889",
+    "url": "/sagarp2901/covid-visualizer/static/js/2.77ce6351.chunk.js"
+  },
+  {
+    "revision": "336bf841ad4bc01f1941f937b750d5be",
+    "url": "/sagarp2901/covid-visualizer/static/js/2.77ce6351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0fbbca676867aa7f58",
+    "url": "/sagarp2901/covid-visualizer/static/js/main.53b25a6c.chunk.js"
+  },
+  {
+    "revision": "d21bbf715d31316cda1c",
+    "url": "/sagarp2901/covid-visualizer/static/js/runtime-main.cd84c25b.js"
+  }
+]);
