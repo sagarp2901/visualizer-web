05563164b5ed9874c823e06fa05d678d37a36d35
sagarp2901 <sagar2901@hotmail.com>
2020-03-23 02:44:54 -0400

    Updates

---
+++ precache-manifest.ea6344b42354aa886749cbe438290d2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901335985889c7ae3f64a5db81b1a3e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86da4e2b9badfbd785bb",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "5eccfe2f8251ef894e7b",
+    "url": "/static/css/main.9126b82f.chunk.css"
+  },
+  {
+    "revision": "86da4e2b9badfbd785bb",
+    "url": "/static/js/2.410214a9.chunk.js"
+  },
+  {
+    "revision": "336bf841ad4bc01f1941f937b750d5be",
+    "url": "/static/js/2.410214a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eccfe2f8251ef894e7b",
+    "url": "/static/js/main.f1b80946.chunk.js"
+  },
+  {
+    "revision": "cb04737574853e76debb",
+    "url": "/static/js/runtime-main.e9f87110.js"
+  },
+  {
+    "revision": "9cedadc497d71510c53b590390ce118a",
+    "url": "/static/media/rsz_covid.9cedadc4.png"
+  }
+]);
